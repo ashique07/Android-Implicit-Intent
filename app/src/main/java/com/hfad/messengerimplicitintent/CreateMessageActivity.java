6983f486bd57b1b7b6cbd13faf3a6a43194ae8e9
Ashique Mohaimin <amornab@gmail.com>
2020-02-25 20:10:14 -0500

    A messenger app to demonstrate implicit intent

---
+++ app/src/main/java/com/hfad/messengerimplicitintent/CreateMessageActivity.java
@@ -0,0 +1,30 @@
+package com.hfad.messengerimplicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage (View view)
+    {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        Intent chosenIntent = Intent.createChooser(intent,"Select any application");
+        startActivity(chosenIntent);
+
+
+    }
+}
